457624e8461bef309516e3ac6c9ad7ea8a05c554
elisa33 <pipi.tdfuego@gmail.com>
2022-04-28 11:27:59 -0400

    css details

--- src/components/FichaNutricional.jsx
+++ src/components/FichaNutricional.jsx
@@ -318,7 +318,7 @@ function FichaNutricional() {
                       </svg>
                     </span>malta
                   </label>
-                  <input className="invisible inp-cbx" id="infsotros" type="checkbox" {...register('infusiones.otros')} />
+                  <input className="invisible block inp-cbx" id="infsotros" type="checkbox" {...register('infusiones.otros')} />
                   <label className="cbx alimentos" htmlFor="infsotros"><span>
                       <svg width="12px" height="9px" viewBox="0 0 12 9">
                         <polyline points="1 5 4 8 11 1"></polyline>
@@ -731,49 +731,49 @@ function FichaNutricional() {
               <div className="flex items-center justify-between py-5">
                 <h4>Desayuno</h4>
                 <label className='flex items-center gap-4'>Hora:
-                    <input className='w-20'type="number" {...register('dieta.desayunohora')} />
+                    <input className='w-20' type="number" {...register('dieta.desayunohora')} />
                 </label>
               </div>
               <input className='bottom-line' {...register('dieta.desayuno')} />
               <div className="flex items-center justify-between py-5">
                 <h4>colacion</h4>
                 <label className='flex items-center gap-4'>Hora:
-                    <input className='w-20'type="number" {...register('dieta.colacionhora')} />
+                    <input className='w-20' type="number" {...register('dieta.colacionhora')} />
                 </label>
               </div>
               <input className='bottom-line' {...register('dieta.colacion')} />
               <div className="flex items-center justify-between py-5">
                 <h4>almuerzo</h4>
                 <label className='flex items-center gap-4'>Hora:
-                    <input className='w-20'type="number" {...register('dieta.almuerzohora')} />
+                    <input className='w-20' type="number" {...register('dieta.almuerzohora')} />
                 </label>
               </div>
               <input className='bottom-line' {...register('dieta.almuerzo')} />
               <div className="flex items-center justify-between py-5">
                 <h4>merienda</h4>
                 <label className='flex items-center gap-4'>Hora:
-                    <input className='w-20'type="number" {...register('dieta.meriendahora')} />
+                    <input className='w-20' type="number" {...register('dieta.meriendahora')} />
                 </label>
               </div>
               <input className='bottom-line' {...register('dieta.merienda')} />
               <div className="flex items-center justify-between py-5">
                 <h4>colacion</h4>
                 <label className='flex items-center gap-4'>Hora:
-                    <input className='w-20'type="number" {...register('dieta.colaciondoshora')} />
+                    <input className='w-20' type="number" {...register('dieta.colaciondoshora')} />
                 </label>
               </div>
               <input className='bottom-line' {...register('dieta.colaciondos')} />
               <div className="flex items-center justify-between py-5">
                 <h4>cena</h4>
                 <label className='flex items-center gap-4'>Hora:
-                    <input className='w-20'type="number" {...register('dieta.cenahora')} />
+                    <input className='w-20' type="number" {...register('dieta.cenahora')} />
                 </label>
               </div>
               <input className='bottom-line' {...register('dieta.cena')} />
               <div className="flex items-center justify-between py-5">
                 <h4>otras</h4>
                 <label className='flex items-center gap-4'>Hora:
-                    <input className='w-20'type="number" {...register('dieta.otrashora')} />
+                    <input className='hora' type="number" {...register('dieta.otrashora')} />
                 </label>
               </div>
               <input className='bottom-line' {...register('dieta.otras')} />
