f96c7685ef6f936f0e3e996f2ed56e434df55375
elisa33 <pipi.tdfuego@gmail.com>
2022-04-27 21:25:10 -0400

    button test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,6 +10,7 @@ const Home = () => {
     <div className="bg-green text-white h-screen p-16 flex flex-col items-center gap-5">
         <h2 className="text-white text-center">Hola! soy tu nutricionista, a te pido por favor que completes el siguiente formulario a fin de que....</h2>
         <Link to="/ficha" className="bg-white p-3 rounded-lg text-green">Ficha Nutricional</Link>
+        <Link to="/pacientes" className="bg-white p-3 rounded-lg text-green">pacientes</Link>
     </div>
   )
 }
